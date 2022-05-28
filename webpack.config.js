bc96435292020e426b59342d83ed915a806605d6
herekim <khere220@gmail.com>
2022-05-29 00:35:21 +0900

    output setting

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,17 @@ module.exports = {
   mode: "development",
   //번들링할 파일의 위치
   //기본 파일 위치가 ./src/index.js 이다.
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js", //
+    about: "./src/about.js",
+  },
   //__dirname은 webpack.config.js의 위치
   //두번째 인자는 번들 파일이 형성될 폴더 이름
   output: {
     path: path.resolve(__dirname, "public"),
     //번들링될 파일 이름
-    filename: "bundle.js",
+    //entry의 변수명을 약속된 변수명 name에 할당
+    filename: "[name]_bundle.js",
   },
   module: {
     rules: [
